e9eeb144947efb75b5539c168d25846db195832a
sebastiandrx <sebastianduenas05@gmail.com>
2026-03-04 15:47:50 -0500

    Backend Emulacion

--- app/src/main/java/com/example/bienestapp/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/bienestapp/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.1.6:8080/")
+            .baseUrl("https://backendbienestapp-1.onrender.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
